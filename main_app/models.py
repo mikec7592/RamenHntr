ef3e6481cb992bf89a36ac33edca07bfc73d273b
michael clark <michaelclark@michaels-Air.fios-router.home>
2021-04-01 20:16:38 -0400

    Added a dope ass admin path

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Ramen(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.CharField(max_length=6)
+    description = models.TextField(max_length=380)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.name 
+        
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
+from .models import Ramen
 
 # Create your views here.
-from django.http import HttpResponse
-
-class Ramen:
-    def __init__(self, name, price, description, rating):
-     self.name = name
-     self.price = price
-     self.description = description
-     self.rating = rating 
-
-ramen= [
-    Ramen('Kogane', '$$', 'One of my go to spots', 8),
-    Ramen('Naruto', '$$$', 'Over-rated but good', 6),
-    Ramen('Test Spot', '$$$$', 'Wonder how this is gonna look after some work', 2)
-]
 
 def home(request):
-    return HttpResponse('<h1>YYUUURRRRR</h1>')
+    return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
 def ramen_index(request):
+    ramen = Ramen.objects.all()
     return render(request, 'ramen/index.html', {'ramen': ramen})
